66578a6b9a2fa098e546001e644248a8eba23f2b
Gina Son <ginasunah33@gmail.com>
2017-08-22 16:44:35 -0400

    listing out the current election results and allowing the user to vote

--- election.js
+++ election.js
@@ -1,5 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  var ul = document.getElementById('candidates');
+
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'get',
+    dataType: 'JSON'
+  }).done(function(responseData){
+    for (var i = 0; i < responseData.candidates.length; i++) {
+      var name = responseData.candidates[i].name;
+      var votes = responseData.candidates[i].votes;
+      var li = document.createElement('li');
+      li.innerText = name +':  '+votes;
+      ul.append(li);
+      console.log(name,' ', votes);
+    }
+
+  })
 
 });
