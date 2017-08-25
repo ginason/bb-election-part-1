48feb838220b9281541d8f521cf9743d96751341
Gina Son <ginasunah33@gmail.com>
2017-08-24 20:16:39 -0400

    add location reload

--- election.js
+++ election.js
@@ -2,14 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var ul = document.getElementById('candidates');
   var form = document.querySelector('.can-form');
-  var submit = document.getElementById('submit');
 
 
-  $.ajax({
-    url: 'https://bb-election-api.herokuapp.com/',
-    method: 'get',
-    dataType: 'JSON'
-  }).done(function(responseData){
+  function list(responseData) {
     for (var i = 0; i < responseData.candidates.length; i++) {
       var name = responseData.candidates[i].name;
       var votes = responseData.candidates[i].votes;
@@ -24,7 +19,16 @@ document.addEventListener("DOMContentLoaded", function() {
       ul.append(li);
       ul.append(vote);
       console.log(name,' ', votes);
-    }
+  }};
+
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'get',
+    dataType: 'JSON'
+  }).done(function(responseData){
+
+    list(responseData);
+    var canList = responseData;
 
     var canVotes = document.querySelectorAll('.vote');
     var canName = document.querySelector('.name');
@@ -45,10 +49,22 @@ document.addEventListener("DOMContentLoaded", function() {
         data: {'id': canId.value, 'name': canName.value},
         dataType: 'json'
       }).done(function(responseData){
-        console.log(this.data);
-        console.log(responseData);
-        var element = document.querySelector('input[type=hidden]').value;
-        console.log(element);
+        // console.log(this.data);
+        // console.log(responseData);
+        // var element = document.querySelector('input[type=hidden]').value;
+        // console.log(element);
+        //
+        // console.log( ul );
+        ul.innerHTML = '';
+
+        // for (var i = 0; i < canList.candidates.length; i++) {
+        //   canList.candidates[i].
+        // }
+        // if (canList.candidates) {
+        //
+        // }
+        location.reload();
+        list(canList);
 
       }).fail(function(xhr, textStatus, errorThrown){
         alert(xhr.responseText);
